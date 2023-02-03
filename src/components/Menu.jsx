08df35601261ea8d65bfaced01fa6f346efba8ad
Idan Fischer <Idanfischer5@gmail.com>
2023-02-03 11:03:22 -0500

    made a Café website with react

---
+++ src/components/Menu.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+export default function Menu() {
+const [coffees, setCoffees] = useState()
+const getCoffees = (temp) => {
+  fetch(`https://api.sampleapis.com/coffee/${temp}`)
+  .then(res => res.json())
+  .then(setCoffees)
+  .catch(alert)
+  
+}
+  return (
+    <article>
+      <div>
+        <button onClick={() => getCoffees('hot')}> Hot Coffees</button>
+        <button onClick={() => getCoffees('iced')}>Iced Coffees</button>
+      </div>
+      {!coffees
+      ?<h2>Welcome to the Beau Café</h2>
+      : <ul>
+        {coffees.map( coffee => (
+          <li key={coffee.id}>{coffee.title}</li>
+        )
+          
+        )}
+      </ul>
+
+      
+      }
+    </article>
+    );
+}
